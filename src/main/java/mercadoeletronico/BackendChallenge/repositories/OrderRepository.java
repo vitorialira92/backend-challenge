0350fa1459a4aa5c9e35b6478895fdeaa7746aa1
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-04-26 16:44:28 -0300

    feat: order repository interface

---
+++ src/main/java/mercadoeletronico/BackendChallenge/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package mercadoeletronico.BackendChallenge.repositories;
+
+
+import mercadoeletronico.BackendChallenge.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
