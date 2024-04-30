b12881aa4ce67f14b8df9ec2efc696224f01dd8b
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-04-30 16:06:24 -0300

    feat: custom exceptions

---
+++ src/main/java/mercadoeletronico/BackendChallenge/exception/DuplicateCreationAttemptException.java
@@ -0,0 +1,7 @@
+package mercadoeletronico.BackendChallenge.exception;
+
+public class DuplicateCreationAttemptException extends Exception{
+    public DuplicateCreationAttemptException(Long id, String resourceName){
+
+    }
+}
---
+++ src/main/java/mercadoeletronico/BackendChallenge/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package mercadoeletronico.BackendChallenge.exception;
+
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String resourceName){
+
+    }
+}
