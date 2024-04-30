e20b65a318915b278751f393f0baf670fee97487
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-04-30 17:20:30 -0300

    feat: order service test file

---
+++ src/test/java/mercadoeletronico/BackendChallenge/services/OrderServiceTest.java
@@ -0,0 +1,44 @@
+package mercadoeletronico.BackendChallenge.services;
+
+import mercadoeletronico.BackendChallenge.domain.Order;
+import mercadoeletronico.BackendChallenge.exception.ResourceNotFoundException;
+import mercadoeletronico.BackendChallenge.repositories.ItemRepository;
+import mercadoeletronico.BackendChallenge.repositories.OrderItemRepository;
+import mercadoeletronico.BackendChallenge.repositories.OrderRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private OrderItemRepository orderItemRepository;
+    @Mock
+    private ItemRepository itemRepository;
+    @InjectMocks
+    private OrderService orderService;
+
+    @Test
+    public void testFindOrderById() throws ResourceNotFoundException {
+        Order expectedOrder = new Order();
+        expectedOrder.setId(1L);
+
+        when(orderRepository.findById(1L)).thenReturn(Optional.of(expectedOrder));
+
+        Order actualOrder = orderService.getOrderById(1L);
+
+        assertNotNull(expectedOrder);
+        assertEquals(expectedOrder.getId(), actualOrder.getId());
+    }
+
+}
