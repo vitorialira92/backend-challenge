865d320bcf4df18fe4a7517346f3dd9e4d9627c9
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-04-30 16:05:13 -0300

    feat: StatusChangeResponseDTO

--- src/main/java/mercadoeletronico/BackendChallenge/dtos/status_change/StatusChangeResponseDTO.java
+++ src/main/java/mercadoeletronico/BackendChallenge/dtos/status_change/StatusChangeResponseDTO.java
@@ -2,7 +2,10 @@ package mercadoeletronico.BackendChallenge.dtos.status_change;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.util.List;
+
 @Schema(description = "Response to the change of status of an order")
 
 public class StatusChangeResponseDTO {
+    public List<String> status;
 }
