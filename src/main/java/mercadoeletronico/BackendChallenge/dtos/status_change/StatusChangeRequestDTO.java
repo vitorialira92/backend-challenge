787a6493793a3e0ca46452f5b8fa8d459e636a3b
vitorialira92 <vitoriabusinesscontact@gmail.com>
2024-04-30 16:04:50 -0300

    fix: wrong variable name

--- src/main/java/mercadoeletronico/BackendChallenge/dtos/status_change/StatusChangeRequestDTO.java
+++ src/main/java/mercadoeletronico/BackendChallenge/dtos/status_change/StatusChangeRequestDTO.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Input to change an order's status")
 public class StatusChangeRequestDTO {
     public StatusChange status;
-    public int itemsAprovados;
+    public int itensAprovados;
     public double valorAprovado;
     public Long pedido;
 }
